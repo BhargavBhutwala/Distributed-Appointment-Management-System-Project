7675476556fe0e224793e1efe23c8b89487b6792
BhargavBhutwala <79571103+BhargavBhutwala@users.noreply.github.com>
2022-04-02 20:20:42 -0400

    Add files via upload

---
+++ Comp6231_Project/src/Constants/Constants.java
@@ -0,0 +1,46 @@
+package Constants;
+
+public class Constants {
+    public static final String FRONTEND_IP="132.205.95.131";
+    public static final String FAIL_RM1_IP="132.205.95.131";
+    public static final String FAIL_RM2_IP="132.205.95.132";
+    public static final String FAIL_RM3_IP="132.205.95.133";
+    public static final String MULTICAST_IP="230.0.0.0";
+    public static final int RM_FRONTEND_PORT=9990;
+    public static final int RM1_FRONTEND_PORT=9990;
+    public static final int FAULT_PORT=6467;
+    public static final String FAULT_MULTICAST_IP="230.2.2.5";
+    public static final int BYTE_LENGTH=65535;
+    public static final int RM_Montreal_PORT = 6000;
+    public static final int RM_Quebec_PORT = 3300;
+    public static final int RM_Sherbrook_PORT = 4200;
+    public static final int LOCAL_Montreal_PORT = 2400;
+    public static final int LOCAL_Quebec_PORT = 3000;
+    public static final int LOCAL_Sherbrook_PORT = 9000;
+    public static final int FAIL_Montreal_PORT = 7990;
+    public static final int FAIL_Quebec_PORT = 7991;
+    public static final int FAIL_Sherbrook_PORT = 7992;
+    public static final String OPERATION_STATUS = "status";
+    public static final String ADD_OPERATION = "addAppointment";
+    public static final String REMOVE_OPERATION = "removeAppointment";
+    public static final String LIST_OPERATION = "listAppointment";
+    public static final String BOOK_OPERATION = "bookAppointment";
+    public static final String CANCEL_OPERATION = "cancelAppointment";
+    public static final String SCHEDULE_OPERATION = "scheduleAppointment";
+    public static final String SWAP_OPERATION = "swapAppointment";
+    public static final String NONE = "None";
+    public static final String LOCALHOST = "localhost";
+    public static final String ID = "id";
+    public static final String APPOINTMENT_ID = "appointId";
+    public static final String APPOINTMENT_TYPE = "appointType";
+    public static final String APPOINTMENT_CAPACITY = "appointCapacity";
+    public static final String OLD_APPOINTMENT_ID = "oldAppointId";
+    public static final String OLD_APPOINTMENT_TYPE = "oldAppointType";
+    public static final String OPERATION = "operation";
+    public static final String LIST_APPOINTMENT_AVAILABLE = "listAppointment";
+    public static final String LIST_APPOINTMENT_SCHEDULE = "scheduleAppointment";
+    public static final String RM1_ID = "RM1";
+    public static final String RM2_ID = "RM2";
+    public static final String RM3_ID = "RM3";
+
+}
